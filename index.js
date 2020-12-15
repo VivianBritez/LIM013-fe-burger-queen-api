94e23dd0a9c456122413530d685d7e9de63d3dcf
Illari Alvarez-Gil <friedapples824qgmail.com>
2020-12-15 10:31:15 -0500

    cambios iniciales

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
@@ -7,12 +8,23 @@ const pkg = require('./package.json');
 
 const { port, dbUrl, secret } = config;
 const app = express();
-
 // TODO: Conexión a la Base de Datos (MongoDB o MySQL)
-
+const connection = mysql.createConnection({
+  host: 'localhost',
+  database: 'burguer_queen',
+  user: 'root',
+  password: 'illari',
+});
 app.set('config', config);
 app.set('pkg', pkg);
-
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('succesful connection');
+  }
+});
+connection.end();
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
