444eec40817f58ca759bea2d5382a03036fece38
vivianBritez <vivianbritez91@gmail.com>
2020-12-15 10:31:01 -0500

    closes #1; connection to the database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const { database } = require('./database.js');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
@@ -8,41 +9,32 @@ const pkg = require('./package.json');
 
 const { port, dbUrl, secret } = config;
 const app = express();
-
 // TODO: Conexión a la Base de Datos (MongoDB o MySQL)
-const conexion = mysql.createConnection({
-  host: 'localhost',
-  database: 'burger__queen',
-  user: 'root',
-  password: 'sql12345',
-});
-
+const conexion = mysql.createConnection(database);
 conexion.connect((error) => {
   if (error) {
     throw error;
   } else {
-    console.log('CONEXION EXITOSAaaa');
+    // parse application/x-www-form-urlencoded
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(authMiddleware(secret));
+    app.set('config', config);
+    app.set('pkg', pkg);
+    // Registrar rutas
+    routes(app, (err) => {
+      if (err) {
+        throw err;
+      }
+
+      app.use(errorHandler);
+
+      app.listen(port, () => {
+        console.info(`App listening on port ${port}`);
+      });
+    });
+    console.log('conexion exitosa');
   }
 });
+
 conexion.end();
-
-app.set('config', config);
-app.set('pkg', pkg);
-
-// parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(authMiddleware(secret));
-
-// Registrar rutas
-routes(app, (err) => {
-  if (err) {
-    throw err;
-  }
-
-  app.use(errorHandler);
-
-  app.listen(port, () => {
-    console.info(`App listening on port ${port}`);
-  });
-});
